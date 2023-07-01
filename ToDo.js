e022e3576994c8b3b9eaabeab708a3be59d7323e
KAA1234 <94076913+KAA1234@users.noreply.github.com>
2023-07-01 14:15:00 +0000

    added ToDo

--- ToDo.js
+++ ToDo.js
@@ -1,1 +1,16 @@
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+var toDoArray = ["test"];
+
+function addToDo(){
+  var toDoItem = document.getElementById("toDo").value;
+  toDoArray.push(toDoItem)
+}
+  
+function updateToDo(){
+  let toDoArray =["one","cool"];
+  console.log(toDoArray)
+  let list = document.getElementById("UnList");
+  console.log(list)
+  for (var i = 0; i < toDoArray.length; i++){
+    list.innerHTML += toDoArray[i]}}
